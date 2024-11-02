8dbeccbb1ec9bc6a6fc60088d3363098c79c9f33
seoudimohamedsaid <saidsandero479@gmail.com>
2024-11-02 21:27:32 +0100

    Create Lab3Exo1.js

---
+++ Lab3Exo1.js
@@ -0,0 +1,13 @@
+// echo.js
+
+// Define the exf function
+function exf(s, n) {
+    // Print the string s, n times
+    for (let i = 0; i < n; i++) {
+        console.log(s);
+    }
+}
+
+// Call the function with the specified arguments
+exf("echo", 5);
+exf("JS from server", 10);
